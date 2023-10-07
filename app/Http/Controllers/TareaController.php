d5bd0a75a8a31effe3d844a674563bcecaa4287b
EnriqueSole <enriquesole23994@gmail.com>
2023-10-07 18:55:05 -0300

    Creando funciones de Leer y Crear Tareas

--- app/Http/Controllers/TareaController.php
+++ app/Http/Controllers/TareaController.php
@@ -4,8 +4,34 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Tarea;
+use Illuminate\Support\Facades\Validator;
 
 class TareaController extends Controller
 {
-    //
+    public function Crear(Request $request){
+        $Tarea=new Tarea;
+        $Validaciones= Validator::make($request->all(),[
+            "titulo"=>"required",
+            "contenido"=>"required",
+            "estado"=>"required",
+            "autor"=>"required",
+        ]);
+       if ($Validaciones->fails()){
+        return response([$Validaciones->errors()], 400 );
+       }
+       $Tarea->titulo=$request->titulo;
+       $Tarea->contenido=$request->contenido;
+       $Tarea->estado=$request->estado;
+       $Tarea->autor=$request->autor;
+       $Tarea->save();
+       return $Tarea;
+    }
+
+    public function Leer($idTarea) {
+        $Tarea=Tarea::find($idTarea);
+        if (!$Tarea) return response(["Mensaje" =>"Tarea no encontrada"], 404);
+        return $Tarea;
+    }
+    
 }
